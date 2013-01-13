a9ae6e283aba4e5e033e4b629b3e93abb108fc08
Mark Slater <mark.slater@mail.com>
2013-01-13 17:10:15 +0000

    Mark - Turn down logging.

--- src/test/jsonos/ConnectionTest.java
+++ src/test/jsonos/ConnectionTest.java
@@ -1,5 +1,9 @@
 package jsonos;
 
+import com.google.common.base.Predicate;
+import com.google.common.base.Supplier;
+import com.google.common.collect.Iterables;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.teleal.cling.UpnpService;
 import org.teleal.cling.UpnpServiceImpl;
@@ -19,7 +23,39 @@ import org.teleal.cling.registry.DefaultRegistryListener;
 import org.teleal.cling.registry.Registry;
 import org.teleal.cling.registry.RegistryListener;
 
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Handler;
+import java.util.logging.Logger;
+
+import static com.google.common.collect.Iterables.any;
+import static java.util.Arrays.asList;
+import static java.util.logging.Level.WARNING;
+
 public class ConnectionTest {
+
+    @BeforeClass
+    public static void setLogLevel() {
+        final Logger topLogger = java.util.logging.Logger.getLogger("");
+        find(asList(topLogger.getHandlers()), new Predicate<Handler>() {
+                    @Override
+                    public boolean apply(Handler input) {
+                        return input instanceof ConsoleHandler;
+                    }
+                }, new Supplier<Handler>() {
+                    @Override
+                    public Handler get() {
+                        Handler handler = new ConsoleHandler();
+                        topLogger.addHandler(handler);
+                        return handler;
+                    }
+                }
+        ).setLevel(WARNING);
+    }
+
+    private static <T> T find(Iterable<T> source, Predicate<T> predicate, Supplier<T> defaultSupplier) {
+        return (any(source, predicate)) ? Iterables.find(source, predicate) : defaultSupplier.get();
+    }
+
     @Test
     public void canDoSomething() throws Exception {
         // UPnP discovery is asynchronous, we need a callback
