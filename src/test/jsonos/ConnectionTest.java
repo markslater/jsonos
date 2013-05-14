1ef2be6c271d8b93d4bb17cec0c1ae691e221aaf
Mark Slater <mark.slater@mail.com>
2013-05-14 21:46:29 +0100

    Mark - Remove some stray sysout.

--- src/test/jsonos/ConnectionTest.java
+++ src/test/jsonos/ConnectionTest.java
@@ -6,11 +6,9 @@ public class ConnectionTest {
 
     @Test
     public void findsZonePlayersWithAlarmRegistryListener() throws Exception {
-        System.out.println("Starting Cling...");
         try (final SonosClient sonosClient = new SonosClient(new SysOutAlarmsListener())) {
             Thread.sleep(10000);
         }
-        System.out.println("Stopping Cling...");
     }
 
 }
